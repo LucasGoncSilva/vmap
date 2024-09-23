24cc533871e8d96cf6c054ffc7c7ce7d1e01b0de
Vinefonseca <viniciusfonseca464@gmail.com>
2024-09-23 16:08:01 -0300

    Alteração do Banco Sqlite

--- VMAP/CORE/settings/dev.py
+++ VMAP/CORE/settings/dev.py
@@ -10,14 +10,21 @@ load_dotenv()
 
 
 # docker run --name psql_vmap -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -d postgres
+# DATABASES: dict[str, dict[str, str | Path]] = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": getenv("DATABASE_NAME", "postgres"),
+#         "USER": getenv("DATABASE_USER", "postgres"),
+#         "PASSWORD": getenv("DATABASE_PASSWORD", "postgres"),
+#         "HOST": getenv("DATABASE_HOST", "localhost"),
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES: dict[str, dict[str, str | Path]] = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": getenv("DATABASE_NAME", "postgres"),
-        "USER": getenv("DATABASE_USER", "postgres"),
-        "PASSWORD": getenv("DATABASE_PASSWORD", "postgres"),
-        "HOST": getenv("DATABASE_HOST", "localhost"),
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
