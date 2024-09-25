a729888b8e6834da7c6a9829e1fce0dcbd6dc604
Lucas Silva <lucas.silva@onr.org.br>
2024-09-25 18:54:40 -0300

    Aplica restrição de login para /dashboard

--- VMAP/dashboard/views.py
+++ VMAP/dashboard/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
+@login_required
 def index(req: HttpRequest) -> HttpResponse:
     return render(req, "dashboard/index.html")
