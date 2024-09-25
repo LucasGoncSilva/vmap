70f27abf0280c318ee14fc35736f515d7387d87c
Lucas Silva <lucas.silva@onr.org.br>
2024-09-25 18:59:55 -0300

    Fluxo inicial da página /cadastro

--- VMAP/account/urls.py
+++ VMAP/account/urls.py
@@ -9,4 +9,5 @@ app_name: Final[str] = "account"
 urlpatterns: list[URLPattern] = [
     path("", views.login, name="empty_login"),
     path("login", views.login, name="login"),
+    path("cadastro", views.register, name="register"),
 ]
