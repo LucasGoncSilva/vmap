2240af13cba6582467fb76c888f1d43e7416a68b
Lucas Silva <lucas.silva@onr.org.br>
2024-10-03 17:09:31 -0300

    Fluxo de login finalizado

--- VMAP/account/views.py
+++ VMAP/account/views.py
@@ -3,7 +3,7 @@ from typing import Final, cast
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.messages import error, success, warning
+from django.contrib.messages import error, success
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -12,10 +12,9 @@ from django.urls import reverse
 INVALID_PASSWORDS: Final[str] = 'Senhas incompatíveis ou menores de 8 caracteres.'
 INVALID_EMAIL: Final[str] = 'Email inválido.'
 INVALID_USERNAME: Final[str] = 'Username inválido.'
-INVALID_NAMES: Final[str] = 'Nome/sobrenome inválidos.'
+INVALID_NAMES: Final[str] = 'Nome/sobrenome inválido.'
 USER_CREATED: Final[str] = 'Usuário criado com sucesso. Informe um ADM para ativá-lo.'
 INVALID_CREDENTIALS: Final[str] = 'Credenciais inválidas.'
-USER_NOT_FOUND: Final[str] = 'Usuário não encontrado.'
 USER_INACTIVE: Final[str] = 'Usuário inativo.'
 
 
@@ -26,10 +25,10 @@ def register_view(req: HttpRequest) -> HttpResponse:
     elif req.method != 'POST':
         return render(req, 'account/register.html')
 
-    email: str | None = req.POST.get('email')
-    username: str | None = req.POST.get('username')
     first: str | None = req.POST.get('first')
     last: str | None = req.POST.get('last')
+    username: str | None = req.POST.get('username')
+    email: str | None = req.POST.get('email')
     password: str | None = req.POST.get('password')
     password2: str | None = req.POST.get('password2')
 
@@ -41,20 +40,20 @@ def register_view(req: HttpRequest) -> HttpResponse:
         error(req, INVALID_EMAIL)
         return render(req, 'account/register.html')
 
-    elif User.objects.filter(username=username).exists() or username is None:
-        error(req, INVALID_USERNAME)
-        return render(req, 'account/register.html')
-
     elif first is None or last is None:
         error(req, INVALID_NAMES)
         return render(req, 'account/register.html')
 
+    if username is None or User.objects.filter(username=username).exists():
+        error(req, INVALID_USERNAME)
+        return render(req, 'account/register.html')
+
     user: User = User.objects.create_user(
-        username=username,
-        email=email,
-        first_name=first.strip(),
-        last_name=last.strip(),
-        password=password,
+        username=username.strip(),
+        email=email.strip(),
+        first_name=first.strip().title(),
+        last_name=last.strip().title(),
+        password=password.strip(),
     )
     user.is_active = False
     user.save()
@@ -70,23 +69,22 @@ def login_view(req: HttpRequest) -> HttpResponse:
     elif req.method != 'POST':
         return render(req, 'account/login.html')
 
-    username: str | None = req.POST.get('username')
-    password: str | None = req.POST.get('password')
+    raw_username: str | None = req.POST.get('username')
+    raw_password: str | None = req.POST.get('password')
 
-    if username is None or password is None:
+    if raw_username is None or raw_password is None or len(raw_password) < 8:
         error(req, INVALID_CREDENTIALS)
         return render(req, 'account/login.html')
 
+    username: str = raw_username.strip()
+    password: str = raw_password.strip()
+
     user: User | None = cast(
-        User | None, authenticate(username=username.strip(), password=password.strip())
+        User | None, authenticate(req, username=username, password=password)
     )
 
     if user is None:
-        error(req, USER_NOT_FOUND)
-        return render(req, 'account/login.html')
-
-    elif not user.is_active:
-        warning(req, USER_INACTIVE)
+        error(req, INVALID_CREDENTIALS)
         return render(req, 'account/login.html')
 
     login(req, user)
